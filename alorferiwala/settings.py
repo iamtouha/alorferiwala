0ed9b0b779a4055dada38df45816ead30bd2e8d7
Touha Zohair <touha98@gmail.com>
2019-05-10 03:58:50 +0600

    whitenoise storageremoved h-18

--- alorferiwala/settings.py
+++ alorferiwala/settings.py
@@ -61,10 +61,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alorferiwala.wsgi.application'
 
-#whitenoise
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 DATABASES = {
     'default': {
         'NAME': 'alorferiwala',
