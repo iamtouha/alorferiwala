5ca005fc36d9c16613acbd6a23081867d9b60a4d
Touha Zohair <touha98@gmail.com>
2019-05-09 23:33:53 +0600

    local settings edited

--- alorferiwala/settings.py
+++ alorferiwala/settings.py
@@ -1,19 +1,14 @@
 
 import os
-from .local_settings import key, hosts, db
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = key
-DEBUG = True
-
-ALLOWED_HOSTS = hosts
-
-
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -30,6 +25,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -58,10 +54,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alorferiwala.wsgi.application'
 
+#whitenoise
 
-# Database
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES = db
 
 
 # Password validation
@@ -116,3 +112,8 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
